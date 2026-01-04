8785b66bfe227d17a4646d235bdff717966d7a5d
jahnabigogoi2021-cloud <jahnabigogoi2021@gmail.com>
2026-01-04 22:25:51 +0530

    Refactor server to use environment variable for port

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ mongoose
     await seedBloodInventory(); // 🔥 KEY LINE
   })
   .catch((err) => console.log(err));
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000 - server.js:57");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server running on port ${PORT}');
 });
